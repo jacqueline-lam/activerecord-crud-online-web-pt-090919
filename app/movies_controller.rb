28053ef019f16b3fcd5966c4847c50fecd7d3785
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-12-01 22:50:22 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Replace the '__' in the below methods to to make the specs pass!
 # Each '__' corresponds to a single line of code you will need to write.
 # See the example below on the left and how it should look on the right.
@@ -50,7 +51,7 @@ def can_get_the_last_item_in_the_database
 end
 
 def can_get_size_of_the_database
-  Movie.size
+  Movie.count
 end
 
 def can_find_the_first_item_from_the_database_using_id
@@ -69,7 +70,8 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-  Movie.order(:release_date).reverse
+  
+  Movie.where('release_date > 2002').order('release_date desc')
 end
 
 # UPDATE
@@ -86,7 +88,7 @@ def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
   update_movie = Movie.find_by(title: "Wat?")
-  update_movie.update(name: "Wat, huh?")
+  update_movie.update(title: "Wat, huh?")
 end
 
 def can_update_multiple_items_at_once
@@ -100,13 +102,13 @@ end
 # DESTROY
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
-  __
+  destroy_movie = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
+  destroy_movie.destroy
 end
 
 def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
   end
-  __
+  Movie.destroy_all()
 end

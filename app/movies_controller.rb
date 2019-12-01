f89ac140fb7dc5b01487eaa9ad6fb9df3fca40e8
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-12-01 22:27:56 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -70,6 +70,7 @@ def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
   # Movie.order(:release_date).reverse
+  bindng.pry
   Movie.where('release_date > 2000').order('release_date desc')
 end
 
